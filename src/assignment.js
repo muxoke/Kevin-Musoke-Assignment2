edbfd90013247fc0b88424768a95b140cb1857c2
Kevin <>
2022-07-08 08:45:48 +0000

    Wrote a function to add numbers in the given array and another that counts the even numbers in the same array.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,17 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+
+const sumOfNumbers = (arrayOfNumbers) => {
+  let sum = 0;
+  for (let number of arrayOfNumbers){
+    sum += number;
+  }
+  return sum;
+  // console.log(arrayOfNumbers)
+};
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const result = [];
+  for (let i = 0; i <= arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] % 2 === 0) result.push(arrayOfNumbers[i]);
+  }
+  // console.log(result)
+    return result.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
